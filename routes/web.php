70b526e9a103237141f51feca6fcd615e5c07c37
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2018-12-11 06:06:14 +0700

    api auth

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,4 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home/{view?}', 'HomeController@index')->where('view', '(.*)')->name('home');
