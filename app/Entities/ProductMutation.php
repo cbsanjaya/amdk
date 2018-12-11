29a74d2798a6d274fcea1b9e91423ad59c422a9e
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2018-12-11 22:27:29 +0700

    ProductMutation model

---
+++ app/Entities/ProductMutation.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Entities;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductMutation extends Model
+{
+    /**
+     * Indicates if the model should be timestamped.
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+}
---
+++ database/migrations/2018_12_11_150139_create_product_mutations_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductMutationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_mutations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('product_id');
+            $table->string('reference')
+                ->comment('referensi ke transaksi jenis:id');
+            $table->decimal('debit', 13, 2)->default(0);
+            $table->decimal('credit', 13, 2)->default(0);
+            $table->timestamp('at');
+            $table->foreign('product_id')
+                ->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_mutations');
+    }
+}
