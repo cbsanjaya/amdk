a784833d17c69eb7f336f39c9cee24b82478cbaa
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2018-12-13 06:51:43 +0700

    assemblecontroller and testing

--- app/Http/Controllers/AssembleController.php
+++ app/Http/Controllers/AssembleController.php
@@ -49,6 +49,7 @@ class AssembleController extends Controller
 
             $mutation = new ProductMutation();
             $mutation->product_id = $request->product_id;
+            $mutation->group = 'assemble:'.$assemble->id;
             $mutation->reference = 'assemble:'.$assemble->id;
             $mutation->quantity = $request->quantity;
             $mutation->period = substr($request->transaction_time, 0, 7);
@@ -65,6 +66,7 @@ class AssembleController extends Controller
 
                 $mutation = new ProductMutation();
                 $mutation->product_id = $item['product_id'];
+                $mutation->group = 'assemble:'.$assemble->id;
                 $mutation->reference = 'assemble_product:'.$assembleProduct->id;
                 $mutation->quantity = -$item['quantity'];
                 $mutation->period = substr($request->transaction_time, 0, 7);
@@ -88,7 +90,7 @@ class AssembleController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function show($assemble)
+    public function show(Assemble $assemble)
     {
         return $assemble;
     }
@@ -103,7 +105,60 @@ class AssembleController extends Controller
      */
     public function update(Request $request, Assemble $assemble)
     {
-        //
+        $assemble = DB::transaction(function () use ($request, $assemble) {
+            $products = collect($request->products);
+
+            $newProductPrice = $products->sum(function ($item) {
+                return $item['price'] * $item['quantity'];
+            });
+
+            $assembleProducts = $assemble->assembleProducts()->delete();
+            
+            $assemble->type = $request->type;
+            $assemble->transaction_time = $request->transaction_time;
+            $assemble->product_id = $request->product_id;
+            $assemble->quantity = $request->quantity;
+            $assemble->price = $newProductPrice;
+            $assemble->memo = $request->memo;
+            $assemble->user_id = $request->user_id;
+            $assemble->save();
+
+            ProductMutation::where('group', 'assemble:'.$assemble->id)->delete();
+
+            $mutation = new ProductMutation();
+            $mutation->product_id = $request->product_id;
+            $mutation->group = 'assemble:'.$assemble->id;
+            $mutation->reference = 'assemble:'.$assemble->id;
+            $mutation->quantity = $request->quantity;
+            $mutation->period = substr($request->transaction_time, 0, 7);
+            $mutation->at = $request->transaction_time;
+            $mutation->save();
+            
+            foreach ($request->products as $item) {
+                $assembleProduct = new AssembleProduct();
+                $assembleProduct->assemble_id = $assemble->id;
+                $assembleProduct->product_id = $item['product_id'];
+                $assembleProduct->quantity = $item['quantity'];
+                $assembleProduct->price = $item['price'];
+                $assembleProduct->save();
+
+                $mutation = new ProductMutation();
+                $mutation->product_id = $item['product_id'];
+                $mutation->group = 'assemble:'.$assemble->id;
+                $mutation->reference = 'assemble_product:'.$assembleProduct->id;
+                $mutation->quantity = -$item['quantity'];
+                $mutation->period = substr($request->transaction_time, 0, 7);
+                $mutation->at = $request->transaction_time;
+                $mutation->save();
+            }
+
+            return $assemble;
+        });
+
+        return [
+            'message'  => 'Perakitan Berhasil diupdate',
+            'assemble' => $assemble,
+        ];
     }
 
     /**
@@ -115,8 +170,17 @@ class AssembleController extends Controller
      */
     public function destroy(Assemble $assemble)
     {
-        $assemble->delete();
+        $assemble = DB::transaction(function () use ($assemble) {
+            ProductMutation::where('group', 'assemble:'.$assemble->id)->delete();
+            $assemble->assembleProducts()->delete();
+            $assemble->delete();
 
-        return $assemble;
+            return $assemble;
+        });
+
+        return [
+            'message'  => 'Perakitan Berhasil dihapus',
+            'assemble' => $assemble,
+        ];
     }
 }
--- tests/Feature/AssembleTest.php
+++ tests/Feature/AssembleTest.php
@@ -11,12 +11,26 @@ class AssembleTest extends TestCase
 
     public function testGetAssembles()
     {
-        $this->assertTrue(true);
+        $this->seed('AssembleSeeder');
+
+        $response = $this->getJson('/api/assembles');
+
+        $response->assertOk()
+            ->assertJsonFragment(
+                ['id' => 1]
+            );
     }
 
     public function testGetAssemble()
     {
-        $this->assertTrue(true);
+        $this->seed('AssembleSeeder');
+
+        $response = $this->getJson('/api/assembles/1');
+        
+        $response->assertOk()
+            ->assertJson(
+                ['id' => 1,]
+            );
     }
 
     public function testStoreAssemble()
@@ -74,6 +88,7 @@ class AssembleTest extends TestCase
 
         $this->assertDatabaseHas('product_mutations', [
             'product_id' => $product1->id,
+            'group'      => 'assemble:'.$assembleResponse->id,
             'reference'  => 'assemble:'.$assembleResponse->id,
             'quantity'   => $qtyProduct,
             'period'     => substr($transactionTime, 0, 7),
@@ -82,6 +97,7 @@ class AssembleTest extends TestCase
 
         $this->assertDatabaseHas('product_mutations', [
             'product_id' => $product3->id,
+            'group'      => 'assemble:'.$assembleResponse->id,
             'reference'  => 'assemble_product:'. 2,
             'quantity'   => -$qtyRaw2,
             'period'     => substr($transactionTime, 0, 7),
@@ -91,11 +107,123 @@ class AssembleTest extends TestCase
 
     public function testUpdateAssemble()
     {
-        $this->assertTrue(true);
+        $this->seed('AssembleSeeder');
+
+        $qtyProduct = $this->faker->numberBetween(5, 20);
+        $qtyRaw1 = $this->faker->numberBetween(20, 50);
+        $transactionTime = '2018-12-31 07:00:00';
+
+        $product1 = factory('App\Entities\Product')->state('product')->create();
+        $product2 = factory('App\Entities\Product')->state('raw')->create();
+        $newPrice = $product2->cost_price * $qtyRaw1;
+        
+        $response = $this->putJson('/api/assembles/1', [
+            'type'             => 'manual',
+            'transaction_time' => $transactionTime,
+            'product_id'       => $product1->id,
+            'quantity'         => $qtyProduct,
+            'memo'             => 'Assemble Hari ini',
+            'user_id'          => $this->user->id,
+            'products'         => [
+                [
+                    'product_id' => $product2->id,
+                    'quantity'   => $qtyRaw1,
+                    'price'      => $product2->cost_price,
+                ],
+            ],
+        ]);
+
+        $response->assertOk()
+            ->assertJson([
+                'message' => 'Perakitan Berhasil diupdate',
+            ]);
+
+        $assembleResponse = $response->original['assemble'];
+
+        $this->assertDatabaseHas('assembles', [
+            'id'         => $assembleResponse->id,
+            'product_id' => $product1->id,
+            'quantity'   => $qtyProduct,
+            'price'      => $newPrice,
+            'user_id'    => $this->user->id,
+        ]);
+
+        $this->assertDatabaseHas('assemble_products', [
+            'product_id' => $product2->id,
+            'quantity'   => $qtyRaw1,
+            'price'      => $product2->cost_price,
+        ]);
+
+        $this->assertDatabaseMissing('assemble_products', [
+            'product_id' => 2,
+        ]);
+
+        $this->assertDatabaseMissing('assemble_products', [
+            'product_id' => 3,
+        ]);
+
+        $this->assertDatabaseHas('product_mutations', [
+            'product_id' => $product1->id,
+            'group'      => 'assemble:'.$assembleResponse->id,
+            'reference'  => 'assemble:'.$assembleResponse->id,
+            'quantity'   => $qtyProduct,
+            'period'     => substr($transactionTime, 0, 7),
+            'at'         => $transactionTime,
+        ]);
+
+        $this->assertDatabaseHas('product_mutations', [
+            'product_id' => $product2->id,
+            'group'      => 'assemble:'.$assembleResponse->id,
+            'reference'  => 'assemble_product:'. 3,
+            'quantity'   => -$qtyRaw1,
+            'period'     => substr($transactionTime, 0, 7),
+            'at'         => $transactionTime,
+        ]);
+ 
+        $this->assertDatabaseMissing('product_mutations', [
+            'product_id' => 1,
+        ]);
+
+        $this->assertDatabaseMissing('product_mutations', [
+            'product_id' => 2,
+        ]);
+
+        $this->assertDatabaseMissing('product_mutations', [
+            'product_id' => 3,
+        ]);
     }
 
     public function testDeleteAssemble()
     {
-        $this->assertTrue(true);
+        $this->seed('AssembleSeeder');
+
+        $response = $this->deleteJson('/api/assembles/1');
+
+        $response->assertOk()
+            ->assertJson([
+                'message' => 'Perakitan Berhasil dihapus',
+            ]);
+
+        $assembleResponse = $response->original['assemble'];
+
+        $this->assertDatabaseMissing('assembles', [
+            'id' => 1,
+        ]);
+
+        $this->assertDatabaseMissing('assemble_products', [
+            'assemble_id' => 1,
+        ]);
+
+        $this->assertDatabaseMissing('product_mutations', [
+            'product_id' => 1,
+        ]);
+
+        $this->assertDatabaseMissing('product_mutations', [
+            'product_id' => 2,
+        ]);
+
+        $this->assertDatabaseMissing('product_mutations', [
+            'product_id' => 3,
+        ]);
     }
 }
