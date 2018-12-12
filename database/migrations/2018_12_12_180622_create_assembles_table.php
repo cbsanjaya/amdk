0fca2efe431b16555ca2934b3362f5627b97de74
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2018-12-12 19:39:22 +0700

    assemble model

---
+++ database/migrations/2018_12_12_180622_create_assembles_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssemblesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assembles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->enum('type', ['manual', 'auto']);
+            $table->dateTime('transaction_time');
+            $table->unsignedInteger('product_id');
+            $table->unsignedInteger('quantity');
+            $table->string('memo')->nullable();
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assembles');
+    }
+}
---
+++ database/migrations/2018_12_12_181724_create_assemble_products_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssembleProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assemble_products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('assemble_id');
+            $table->unsignedInteger('product_id');
+            $table->unsignedInteger('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assemble_products');
+    }
+}
