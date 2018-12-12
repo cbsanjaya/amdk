9dd6307c733119feb8e77b257233e3398ccbc254
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2018-12-12 19:39:33 +0700

    Assemble result

---
+++ database/migrations/2018_12_12_193121_create_assemble_results_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAssembleResultsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assemble_results', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('assemble_id');
+            $table->dateTime('transaction_time');
+            $table->unsignedInteger('product_id');
+            $table->unsignedInteger('quantity');
+            $table->string('memo')->nullable();
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assemble_results');
+    }
+}
