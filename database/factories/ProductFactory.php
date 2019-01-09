9d91db88f720682dc3579c48df869f6df6fe12e6
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2019-01-09 15:41:35 +0700

    product crud

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,7 +2,7 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(App\Entities\Product::class, function (Faker $faker) {
+$factory->define(App\Models\Product::class, function (Faker $faker) {
     $price = $faker->numberBetween(1000, 3000);
 
     return [
@@ -16,13 +16,13 @@ $factory->define(App\Entities\Product::class, function (Faker $faker) {
     ];
 });
 
-$factory->state(App\Entities\Product::class, 'product', [
+$factory->state(App\Models\Product::class, 'product', [
     'tobuy'  => false,
     'tosell' => true,
     'raw'    => false,
 ]);
 
-$factory->state(App\Entities\Product::class, 'raw', [
+$factory->state(App\Models\Product::class, 'raw', [
     'tobuy'  => true,
     'tosell' => false,
     'raw'    => true,
