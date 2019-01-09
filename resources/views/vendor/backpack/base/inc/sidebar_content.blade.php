479ef486e3a9d6e448b29ff75925fffdb3ffe94b
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2019-01-09 22:17:39 +0700

    cleaning route agar bisa di cache

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,7 +1,7 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard"></i> <span>{{ trans('backpack::base.dashboard') }}</span></a></li>
 
-<li><a href='{{ backpack_url('product') }}'><i class='fa fa-shopping-cart'></i> <span>Product</span></a></li>
+<li><a href="{{ backpack_url('product') }}"><i class='fa fa-shopping-cart'></i> <span>Product</span></a></li>
 
 <!-- Users, Roles Permissions -->
 <li class="treeview">
