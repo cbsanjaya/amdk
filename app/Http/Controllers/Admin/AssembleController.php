a66aa2d9cad977c7b4b12f444282bf8275d38792
Cahya Bagus Sanjaya <9c96b6@gmail.com>
2019-01-17 17:49:21 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- app/Http/Controllers/Admin/AssembleController.php
+++ app/Http/Controllers/Admin/AssembleController.php
@@ -71,7 +71,7 @@ class AssembleController extends CrudController
             'name' => 'product_id',
             'type' => 'select2',
             'label' => 'Nama Product',
-            'options' => function($model) {
+            'options' => function ($model) {
                 return $model->where('tosell', true)->where('raw', false)->get();
             },
             'entity' => 'product', // the method that defines the relationship in your Model
@@ -107,7 +107,7 @@ class AssembleController extends CrudController
                     'attribute' => 'description',
                     'size' => '3',
                     'model' => "App\Models\Product",
-                    'options' => function($model) {
+                    'options' => function ($model) {
                         return $model->where('raw', true)->get();
                     },
                 ],
